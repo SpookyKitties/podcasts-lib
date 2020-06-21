cb6b3a25708e7ccd795453cf6ba2a12f318ec7fd
Jared Parkinson <jared@parkinson.im>
2020-06-21 01:45:52 -0600

    init

---
+++ src/Podcast.ts
@@ -0,0 +1,26 @@
+export class Podcast {
+  public title: string;
+  public homepageLink: string;
+  public language: string;
+  public copyright: string;
+  public subtitle: string;
+  public author: string;
+  public summary: string;
+  public description: string;
+  public explicit: boolean;
+  public type: string;
+  public keywords: string;
+  public image: string;
+  public category: string[];
+}
+
+export class PodcastOwner {
+  public name?: string;
+  public email?: string;
+}
+
+export class PodcastImage {
+  public url: string;
+  public title: string;
+  public link: string;
+}
